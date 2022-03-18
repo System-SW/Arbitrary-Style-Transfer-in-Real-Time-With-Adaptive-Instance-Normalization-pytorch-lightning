e709803be4d164a7fe111733b039357c4d54e511
yslee <yslee.dev@gmail.com>
2022-03-19 01:36:49 +0900

    fix: remove transform test

--- tests/test_dataset.py
+++
@@ -1,7 +0,0 @@
-from dataset import Transforms
-
-
-def test_transforms(args, image_batch):
-    T = Transforms(args.image_size)
-    batch = T(image_batch)
-    assert list(batch.shape) == [3, args.image_size, args.image_size]
