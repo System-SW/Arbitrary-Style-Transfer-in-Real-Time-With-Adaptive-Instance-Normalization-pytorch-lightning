b8d7c0521dd885b95e5868433a95e30017941831
yslee <yslee.dev@gmail.com>
2022-03-20 12:38:16 +0900

    feat: streamlit app

---
+++ app/utils.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import cv2
+
+TITLE = (
+    "Arbitrary Style Transfer in Real-time"
+    "with Adaptive Instance Normalization"
+)
+
+DESC = "Unofficial pytorch-lightning implement"
+
+AUTHOR = "YSLEE(rapidrabbit76)"
+
+URL = (
+    "https://github.com/rapidrabbit76/"
+    "Arbitrary-Style-Transfer-in-Real-Time-"
+    "With-Adaptive-Instance-Normalization-pytorch-lightning"
+)
+
+MODEL_PATH = os.environ.get("MODEL_PATH")
+
+EXT = ["png", "jpeg", "jpg"]
+
+
+def image_load(file):
+    image = cv2.imdecode(np.asarray(bytearray(file.read()), dtype=np.uint8), 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
